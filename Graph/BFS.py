a3ddc40a66b86e5e6537089e0f01d10a1bac2fe1
Dileep Pandey <dileep.pandey@asu.edu>
2019-04-13 01:05:06 -0700

    BFS

--- Graph/BFS.py
+++ Graph/BFS.py
@@ -1,16 +1,45 @@
 """
 BFS traversal of a graph
 """
-from collections import deque
+from collections import deque, OrderedDict
 
+class BFSResult:
+    def __init__(self):
+        self.level = OrderedDict()
+        self.parent = OrderedDict()
 
-class Solution(object):
+class Graph:
+    def __init__(self):
+        self.adj = {}
 
-    def bfs_traversal(self, root):
-        if root is None:
-            return []
-        Q = deque()
-        Q.append(root)
-        # while len(Q) != 0:
-        #     for i in range(len(Q)):
-        pass
+    def add_edge(self, u, v):
+        if u not in self.adj:
+            self.adj[u] = []
+        self.adj[u].append(v)
+
+def bfs(g, s):
+    """
+    Queue based implementation of BFS
+    @param g: a graph with adjacency list adj such that g.adj[u] us a list of 
+    u's neighbors
+    @param s: source
+    """
+    r = BFSResult()
+    r.level[s] = 0
+    r.parent[s] = None
+
+    i = 1
+    q = deque()
+    q.append(s)
+
+    while len(q) != 0:
+        size = len(q)
+        for i in range(size):
+            u = q.popleft()
+            for v in g.adj[u]:
+                if v not in r.level:
+                    r.level[v] = r.level[u] + 1
+                    r.parent[v] = u
+                    q.append(v)
+    
+    return r
